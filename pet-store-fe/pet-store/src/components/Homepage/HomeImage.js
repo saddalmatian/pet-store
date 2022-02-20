14dbad6fee5d2ed6f1fb2b3de34186a2a8598edd
dth-an29 <dth.an292@gmail.com>
2022-02-20 22:47:52 +0700

    add home

---
+++ pet-store-fe/pet-store/src/components/Homepage/HomeImage.js
@@ -0,0 +1,11 @@
+import './HomeImage.css';
+
+function HomeImage() {
+    return (
+        <div className="col-md">
+            <img className="img-fluid home-img" alt="home-img" src="./assets/images/img-home.png" />
+        </div>
+    );
+}
+
+export default HomeImage;
