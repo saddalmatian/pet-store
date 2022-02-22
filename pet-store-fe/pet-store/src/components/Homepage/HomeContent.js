620b15b835b3dc80eb76911b44fe270ec8534013
dth-an29 <dth.an292@gmail.com>
2022-02-22 16:26:48 +0700

    Add product page

--- pet-store-fe/pet-store/src/components/Homepage/HomeContent.js
+++ pet-store-fe/pet-store/src/components/Homepage/HomeContent.js
@@ -3,7 +3,7 @@ import './HomeContent.css';
 function HomeContent() {
     return (
         <>
-            <p className="mixin-font fs-1">Welcome!</p>
+            <p className="mixin-font">Welcome!</p>
             <p className="home-title">PET STORE <br /> AND BEYOND</p>
             <p className="home-description">Get the best service for your pet or <br /> start shopping now</p>
         </>
