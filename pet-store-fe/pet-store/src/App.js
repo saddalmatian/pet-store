9ed4c1e158fefb2bfa052484176b64e7687aefc3
dth-an29 <dth.an292@gmail.com>
2022-02-22 16:26:48 +0700

    Add product page

--- pet-store-fe/pet-store/src/App.js
+++ pet-store-fe/pet-store/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Homepage/Home';
 import About from './components/About/About';
+import Product from './components/Productpage/Product';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home />
       <About />
+      <Product />
     </div>
   );
 }
