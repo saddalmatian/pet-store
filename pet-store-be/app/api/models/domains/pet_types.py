c3eac7aa64e9a409331534e95e98f5038661f657
saddalmatian <saddalmatian@gmail.com>
2022-02-22 18:10:27 +0700

    Delete dev-be

--- pet-store-be/app/api/models/domains/pet_types.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Field, SQLModel
-
-
-class PetType(SQLModel, table=True):
-    __tablename__ = "pet_type"
-    pet_type_id: str = Field(primary_key=True)
-    pet_type_name: str
