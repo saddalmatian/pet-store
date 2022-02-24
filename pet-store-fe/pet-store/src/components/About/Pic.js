ec2cf2947449db61cd55b733ca6450d2e61fd92b
dth-an29 <dth.an292@gmail.com>
2022-02-24 16:06:13 +0700

    Update about page

--- pet-store-fe/pet-store/src/components/About/Pic.js
+++ pet-store-fe/pet-store/src/components/About/Pic.js
@@ -1,10 +1,10 @@
 import './Pic.css';
-import about from './About.png'
+import AboutImg from '../../assets/images/About.png'
 
 function Pic(options) {
     return(
         <div className="col-md">
-        <img src={about} alt="No pic" className="img-fluid img"></img>
+        <img src={AboutImg} alt="About-images" className="img-fluid about-img"></img>
         </div>
     )
 }
