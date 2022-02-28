07b583762df290fdef71e3b02f9236dd7d67b9f8
saddalmatian <saddalmatian@gmail.com>
2022-02-28 21:10:52 +0700

    update docker

--- pet-store-be/start_docker.sh
+++ pet-store-be/start_docker.sh
@@ -1,3 +1,3 @@
 docker run --name pet_store_sql -e MYSQL_ROOT_PASSWORD=petstore#123A -d mysql:latest
-docker build -t fastapi -f Dockerfile .
+docker build --no-cache -t fastapi -f Dockerfile .	
 docker run -d -t -p 8000:8000 fastapi
