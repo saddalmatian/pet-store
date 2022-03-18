89de9628da29fa74a246fe177c8a14712cc6371b
dth-an29 <dth.an292@gmail.com>
2022-03-18 15:08:14 +0700

    Add router + service detail pages

--- pet-store-fe/pet-store/src/components/Service/ComboSer.js
+++ pet-store-fe/pet-store/src/components/Service/ComboSer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./ComboSer.css";
-import pic1 from "./ser1.png";
-import pic2 from "./ser2.png";
-import pic3 from "./ser3.png";
-import pic4 from "./ser4.png";
-import pic5 from "./All.png";
-import pic6 from "./All2.png";
+import pic1 from "../../assets/images/ser1.png";
+import pic2 from "../../assets/images/ser2.png";
+import pic3 from "../../assets/images/ser3.png";
+import pic4 from "../../assets/images/ser4.png";
+import pic5 from "../../assets/images/All.png";
+import pic6 from "../../assets/images/All2.png";
+
 
 function ComboSer(props){
     return(
@@ -14,7 +15,7 @@ function ComboSer(props){
                 <div className="col-md-3 text-center position-relative p-0">
                     <div class="d-flex flex-column round-3 shadow-lggit service-combo-hover" style={{borderRadius: '15px',backgroundColor: 'var(--primary-color)'}}>
                         <img src={pic1} alt="No-pic" className="img-combo" />
-                        <p className="name-combo col-md-3 text-nowrap">Bathing & Grooming</p>
+                        <p className="name-combo col-md-3 text-nowrap">Tắm & Cắt tỉa</p>
                         <img src={pic2} alt="No-pic" className="img-combo" />
                         
                     </div>
@@ -22,7 +23,7 @@ function ComboSer(props){
                 <div className="col-md-3 text-center position-relative p-0">
                     <div class="d-flex flex-column round-3 shadow-lg service-combo-hover" style={{borderRadius: '15px',backgroundColor: 'var(--primary-color)'}}>
                         <img src={pic3} alt="No-pic" className="img-combo" />
-                        <p className="name-combo col-md-3 text-nowrap">Boarding & Walking</p>
+                        <p className="name-combo col-md-3 text-nowrap">Trông hộ & Đi dạo</p>
                         <img src={pic4} alt="No-pic" className="img-combo" />
                         
                     </div>
@@ -30,7 +31,7 @@ function ComboSer(props){
                 <div className="col-md-3 text-center position-relative p-0">
                     <div class="d-flex flex-column round-3 shadow-lg service-combo-hover" style={{borderRadius: '15px',backgroundColor: 'var(--primary-color)'}}>
                         <img src={pic5} alt="No-pic" className="img-combo" />
-                        <p className="name-combo col-md-3 text-nowrap">All</p>
+                        <p className="name-combo col-md-3 text-nowrap">Tất cả</p>
                         <img src={pic6} alt="No-pic" className="img-combo" />
                     </div>
                 </div> 
---
+++ pet-store-fe/pet-store/src/components/ServiceDetail/BoardingService.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import './ServiceDetail.css';
+import Booking from './Booking';
+import Heading from '../Heading';
+import Hinh from '../../assets/images/boarding.png';
+import Hinh1 from '../../assets/images/boarding1.jpg';
+import Hinh2 from '../../assets/images/boarding2.jpg';
+import Hinh3 from '../../assets/images/boarding3.jpg';
+
+function BoardingService() {
+    return (
+        <div className="container service-detail">
+            <div className="row">
+                <Heading mixin="What we have?" title="Dịch vụ tắm rửa tại Pet Store có gì?" />
+                <div className="col-md image-container">
+                    <div className="img-container">
+                        <img className="service-img" src={Hinh} alt="img"></img>
+                        <img className="service-img" src={Hinh1} alt="img"></img>
+                    </div>
+                    <div className="img-container">
+                        <img className="service-img" src={Hinh2} alt="img"></img>
+                        <img className="service-img" src={Hinh3} alt="img"></img>
+                    </div>
+                </div>
+
+                <div className="col-md service-info">
+                    <div className="content-container">
+                        <p className="service-content">
+                            - Hệ thống phòng 5 sao dành riêng cho thú cưng của bạn.
+                        </p>
+                        <p className="service-content">
+                            - Các bé sẽ được kiểm tra sức khỏe trước khi vào khách sạn 
+                            và cả trong thời gian ở lại đây.
+                        </p>
+                        <p className="service-content">
+                            - Bữa ăn hàng ngày đầy đủ dinh dưỡng đến từ thương hiệu nổi tiếng Eminent.
+                        </p>
+                    </div>
+
+                    <div className="table-container">
+                        <table className="service-table">
+                            <tr className="service-table__row">
+                                <th className="service-table__heading">Weight</th>
+                                <th className="service-table__heading">Price</th>
+                            </tr>
+                            <tr className="service-table__row">
+                                <td className="service-table__data"><i className="fas fa-angle-left"></i> 2kg</td>
+                                <td className="service-table__data">50.000đ/ngày</td>
+                            </tr>
+                            <tr className="service-table__row">
+                                <td className="service-table__data">2 - 4kg</td>
+                                <td className="service-table__data">70.000đ/ngày</td>
+                            </tr>
+                            <tr className="service-table__row">
+                                <td className="service-table__data">4 - 7kg</td>
+                                <td className="service-table__data">100.000đ/ngày</td>
+                            </tr>
+                            <tr className="service-table__row">
+                                <td className="service-table__data">7 - 10kg</td>
+                                <td className="service-table__data">120.000đ/ngày</td>
+                            </tr>
+                            <tr className="service-table__row">
+                                <td className="service-table__data">10 - 15kg</td>
+                                <td className="service-table__data">150.000đ/ngày</td>
+                            </tr>
+                            <tr className="service-table__row">
+                                <td className="service-table__data"><i className="fas fa-angle-right"></i> 15kg</td>
+                                <td className="service-table__data">200.000đ/ngày</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            <Booking />
+        </div>
+    );
+}
+
+export default BoardingService;
