a7e144022da1300f00c7ab35f46951aa4d18c125
Cris17 <trungtvinhl107@gmail.com>
2022-04-05 16:40:44 +0700

    api admin

--- pet-store-fe/pet-store/src/components/Admin/ModalEdit.js
+++ pet-store-fe/pet-store/src/components/Admin/ModalEdit.js
@@ -29,12 +29,12 @@ function ModalEdit(props){
     
     console.log(props.show);
     console.log(products);
-
+    
    
     return (
         
         <Modal {...props}
-            size="xl" 
+            size="lg" 
             aria-labelledby="contained-modal-title-vcenter"
             
         >
@@ -47,27 +47,64 @@ function ModalEdit(props){
                
             <div className="content-dashboard container-fluid">
                 
-                <form>
+                <form >
                         <div className="modal-content form-group d-flex row border-0 gap-4 ">
-                                 <input type="text" className="form-control-lg border " defaultValue={products?.ProductName}/>
-                                <img src={products?.ListImages[0].ImageSource} style={{width: '5%', height: '5%'}}/>
-                                <input type="text" className="form-control-lg border" defaultValue={`${products?.ProductCost} VND`}/>
-                                <input type="text" className="form-control-lg border" defaultValue={products?.ProductQuantity}/>
-                                <input type="text" className="form-control-lg border" defaultValue={products?.RateStarNumber}/>
-                                <input type="file" className="form-control-lg border"/>
-                                <select className="form-control-lg border" >
-                                    <option value ="selected">Choose type...</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
-                                <textarea className="form-control-lg border" defaultValue={products?.ProductDescription}></textarea> 
-                                <select className="form-control-lg border" >
-                                    <option value ="selected">Choose pet type...</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
-                                </select>
+                            <div className="d-flex">
+                            <label htmlFor="product-name" className="label-items">Product Name</label>
+                                <input type="text" className="form-control-lg border input-items product-name "  defaultValue={products?.ProductName}/>
+                            </div>
+                            <div className="d-flex">
+                                <div className="col-md">
+                                    <label htmlFor="product-img" className="label-items">Image</label>
+                                    <input type="file" className="form-control-lg border input-items"/>
+                                </div>
+                            </div>
+                            <div className="d-flex gap-4">
+                                <img src={products?.ListImages[0].ImageSource} style={{width: '80px', height: '80px', marginLeft:"120px"}}/>
+                                <img src={products?.ListImages[0].ImageSource} style={{width: '80px', height: '80px'}}/>
+                                <img src={products?.ListImages[0].ImageSource} style={{width: '80px', height: '80px'}}/>
+                            </div>
+                            <div className="d-flex">
+                                <label htmlFor="product-cost" className="label-items">Product Cost</label>
+                                <input type="text" className="form-control-lg border input-items product-cost"  defaultValue={`${products?.ProductCost} VND`}/>  
+                            </div>
+                            <div className="d-flex">
+                                <label htmlFor="product-quantity" className="label-items">Product Quantity</label>
+                                <input type="number" className="form-control-lg border  product-quantity" defaultValue={products?.ProductQuantity}/>
+
+
+                            </div>
+                            <div className="d-flex">
+                                <label htmlFor="product-rate" className="label-items">Rated</label>
+                                <span className="form-control-lg producst-rate input-item">{products?.RateStarNumber}<i className="fa fa-star text-warning"></i></span>
+
+                            </div>
+                            <div className="d-flex">
+                                <label htmlFor="product-pet-type" className="label-items">Pet type</label>
+                                <input type="text" className="form-control-lg border input-items product-pet-type" defaultValue={products?.PetTypeName}/>
+                            </div>
+                            <div className="d-flex">
+                                <label htmlFor="product-type" className="label-items">Product type</label>
+                                <input type="text" className="form-control-lg border input-items product-type" defaultValue={products?.ProductType}/>
+                            </div>
+                            {/* <input type="file" className="form-control-lg border input-items"/>
+                            <select className="form-control-lg border input-items" >
+                                <option value ="selected">Choose type...</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select> */}
+                            <div className="d-flex">
+                                <label htmlFor="product-desc" className="label-items">Product type</label>
+                                <textarea className="form-control-lg border input-items product-desc" defaultValue={products?.ProductDescription}></textarea> 
+                            </div>
+
+                            {/* <select className="form-control-lg border" >
+                                <option value ="selected">Choose pet type...</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select> */}
                         </div>
                       
                     </form>
@@ -76,7 +113,8 @@ function ModalEdit(props){
    
             </Modal.Body>
             <Modal.Footer>
-                <button  className="btn btn-lg btn-primary"onClick={props.onHide}>Close</button>
+                <button  className="btn btn-lg btn-primary"onClick={props.onHide}>Update</button>
+                <button  className="btn btn-lg btn-secondary"onClick={props.onHide}>Close</button>
                 
             </Modal.Footer>
         </Modal>
--- pet-store-fe/pet-store/src/components/Admin/Sidebar.js
+++ pet-store-fe/pet-store/src/components/Admin/Sidebar.js
@@ -1,65 +1,69 @@
-import React ,{useState}from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
+
 import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
 // Be sure to include styles at some point, probably during your bootstraping
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import "./Sidebar.css";
 
-function Sidebar(){
-    const [selected, setSelected]= useState()
-    return(
-        <SideNav
-        onSelect={(selected) => {
-            setSelected({ selected });
-        }}
-        style={{backgroundColor:"var(--primary-color)", marginBottom:"-100%", paddingBottom:"100%" }}
-    >
-        <SideNav.Toggle style={{color:"#ffff"}} />
-        <SideNav.Nav selected={ selected}>
-            <NavItem eventKey="dashboard">
-                <NavIcon >
-                <Link to="/dashboard"><i className="fa fa-fw fa-home" style={{ fontSize: '1.75em', color:"#ffff" }} /></Link>
-                </NavIcon>
-                <NavText style={{color:"#ffff"}}>
-                    <Link to="/dashboard" style={{ textDecoration:'0'}}>Trang chủ</Link>
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="product">
-                <NavIcon >
-                <Link to="/product"><i className="fa fa-fw fa-box-open" style={{ fontSize: '1.75em', color:"#ffff" }} /></Link>
-                </NavIcon>
-                <NavText style={{color:"#ffff"}}>
-                    <Link to="/product"style={{ textDecoration:'0'}} >  Sản phẩm</Link>
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="bill">
-                <NavIcon >
-                <Link to="/bill" ><i className="fa fa-fw fa-credit-card" style={{ fontSize: '1.75em', color:"#ffff" }} /></Link>
-                </NavIcon>
-                <NavText style={{color:"#ffff"}}>
-                    
-                    <Link to="/bill" style={{ textDecoration:'0'}}>Đơn hàng</Link>
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="promo">
-                <NavIcon >
-                <Link to="/promo"><i className="fa fa-fw fa-tag" style={{ fontSize: '1.75em', color:"#ffff" }} /></Link>
-                </NavIcon>
-                <NavText style={{color:"#ffff"}}>
-                    <Link to="/promo"style={{ textDecoration:'0'}}>Khuyến mãi</Link>
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="staff">
-                <NavIcon >
-                <Link to="/staff"><i className="fa fa-fw fa-user-circle" style={{ fontSize: '1.75em', color:"#ffff" }} /></Link>
-                </NavIcon>
-                <NavText style={{color:"#ffff"}}>
-                    <Link to="/staff"style={{ textDecoration:'0'}}>Nhân viên</Link>
-                </NavText>
-            </NavItem>
-        </SideNav.Nav>
-    </SideNav>
-    
+function Sidebar() {
+    const [selected, setSelected] = useState()
+    return (
+        <React.Fragment>
+            <SideNav
+                onSelect={(selected) => {
+                    setSelected({ selected });
+                }}
+                style={{ backgroundColor: "var(--primary-color)", marginBottom: "-100%", paddingBottom: "100%" }}
+            >
+                <SideNav.Toggle style={{ color: "#ffff" }} />
+                <SideNav.Nav selected={selected}>
+                    <NavItem eventKey="/dashboard">
+                        <NavIcon >
+                            <Link to="/dashboard"><i className="fa fa-fw fa-home" style={{ fontSize: '1.75em', color: "#ffff" }} /></Link>
+                        </NavIcon>
+                        <NavText style={{ color: "#ffff" }}>
+                            <Link to="/dashboard" style={{ textDecoration: '0' }}>Trang chủ</Link>
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="/product">
+                        <NavIcon >
+                            <Link to="/product"><i className="fa fa-fw fa-box-open" style={{ fontSize: '1.75em', color: "#ffff" }} /></Link>
+                        </NavIcon>
+                        <NavText style={{ color: "#ffff" }}>
+                            <Link to="/product" style={{ textDecoration: '0' }} >  Sản phẩm</Link>
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="/bill">
+                        <NavIcon >
+                            <Link to="/bill" ><i className="fa fa-fw fa-credit-card" style={{ fontSize: '1.75em', color: "#ffff" }} /></Link>
+                        </NavIcon>
+                        <NavText style={{ color: "#ffff" }}>
+
+                            <Link to="/bill" style={{ textDecoration: '0' }}>Đơn hàng</Link>
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="/promo">
+                        <NavIcon >
+                            <Link to="/promo"><i className="fa fa-fw fa-tag" style={{ fontSize: '1.75em', color: "#ffff" }} /></Link>
+                        </NavIcon>
+                        <NavText style={{ color: "#ffff" }}>
+                            <Link to="/promo" style={{ textDecoration: '0' }}>Khuyến mãi</Link>
+                        </NavText>
+                    </NavItem>
+                    <NavItem eventKey="/staff">
+                        <NavIcon >
+                            <Link to="/staff"><i className="fa fa-fw fa-user-circle" style={{ fontSize: '1.75em', color: "#ffff" }} /></Link>
+                        </NavIcon>
+                        <NavText style={{ color: "#ffff" }}>
+                            <Link to="/staff" style={{ textDecoration: '0' }}>Nhân viên</Link>
+                        </NavText>
+                    </NavItem>
+                </SideNav.Nav>
+
+            </SideNav>
+        </React.Fragment>
     )
 
 }
