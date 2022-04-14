ee79fa794beb47bc5e54f03f07bab87d0c67e07c
Cris17 <trungtvinhl107@gmail.com>
2022-04-14 16:42:32 +0700

    app

--- pet-store-fe/pet-store/src/App.js
+++ pet-store-fe/pet-store/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route} from 'react-router-dom';
-import Header from './components/Header/Header';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Homepage/Home';
 import About from './components/About/About';
 import Product from './components/Productpage/Product';
@@ -21,12 +20,12 @@ import Admin from './components/Admin/Admin';
 function App() {
   return (
     <div className="App">
-      {/* <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/product' element={<Product />} />
-        <Route path='/product_detail' element={<ProductDetail />} />
+        <Route path='/product/:idType' element={<Product />} />
+        <Route path='/product-list/:petType' element={<Product />} />
         <Route path='/product_detail/:id' element={<ProductDetail />} />
         <Route path='/service' element={<Service />} />
         <Route path='/bathing_service' element={<BathingService />} />
@@ -36,16 +35,12 @@ function App() {
         <Route path='/sign_in' element={<SignIn />} />
         <Route path='/sign_up' element={<SignUp />} />
         <Route path='/cart' element={<Cart />} />
-      </Routes>
-      <Footer /> */}
-
-      <Routes>
         <Route path='/admin' element={<Admin />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
 
 
-      <Admin />
+
     </div>
   );
 }
