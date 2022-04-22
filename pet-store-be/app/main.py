3d8fbe777cfc87ec41436b54e53a1fff81ca537b
saddalmatian <saddalmatian@gmail.com>
2022-04-22 23:05:17 +0700

    Allow all origin

--- pet-store-be/app/main.py
+++ pet-store-be/app/main.py
@@ -26,13 +26,9 @@ app.include_router(dashboards.router)
 app.include_router(rates.router)
 
 
-origins = [
-    "http://localhost:3000",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
